197cbba913ae14e676b767370963a72efc02b368
Rodion <rodson5973@gmail.com>
2025-07-08 21:53:00 +0300

    fix2004

--- server/src/index.ts
+++ server/src/index.ts
@@ -7,7 +7,7 @@ import { connectDB } from "./db.js";
 import taskRoutes from "./routes/tasks";
 import boardRoutes from "./routes/boards";
 
-app.use(express.static(path.join(__dirname, "client")));
+
 
 
 const app = express();
@@ -15,7 +15,7 @@ const PORT = process.env.PORT || 5000;
 
 app.use(cors());
 app.use(express.json());
-
+app.use(express.static(path.join(__dirname, "client")));
 // API
 app.use("/api/tasks", taskRoutes);
 app.use("/api/boards", boardRoutes);
