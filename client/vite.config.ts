5fab6af1b4d4c56474997d5d309f4a010e9399f0
Rodion <rodson5973@gmail.com>
2025-07-08 21:43:47 +0300

    fix2001

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -1,21 +1,13 @@
 // client/vite.config.ts
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
-import { dirname, resolve } from 'path';
-import { fileURLToPath } from 'url';
-
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = dirname(__filename);
-
 export default defineConfig({
   plugins: [react()],
   build: {
-    outDir: '../server/client', // React білд йде прямо в server/client
+    outDir: "dist", // тепер білд піде в /app/client/dist
     emptyOutDir: true,
   },
   resolve: {
     alias: {
-      '@': resolve(__dirname, './src'),
+      "@": resolve(__dirname, "./src"),
     },
   },
 });
