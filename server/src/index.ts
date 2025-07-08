f5782a3277d0c3522a62324e0af13bf86fde7b6e
Rodion <rodson5973@gmail.com>
2025-07-08 18:13:44 +0300

    deploy fix7

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,8 +1,13 @@
 import express from "express";
 import cors from "cors";
+import path from "path";
 import { connectDB } from "./db";
 import taskRoutes from "./routes/tasks";
 import boardRoutes from "./routes/boards";
+import { fileURLToPath } from "url";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
 const app = express();
 const PORT = process.env.PORT || 5000;
@@ -10,12 +15,17 @@ const PORT = process.env.PORT || 5000;
 app.use(cors());
 app.use(express.json());
 
-try {
-  app.use("/api/tasks", taskRoutes);
-  app.use("/api/boards", boardRoutes);
-} catch (err) {
-  console.error("Route setup error:", err);
-}
+
+app.use("/api/tasks", taskRoutes);
+app.use("/api/boards", boardRoutes);
+
+
+app.use(express.static(path.join(__dirname, "client", "dist")));
+
+
+app.get("*", (_, res) => {
+  res.sendFile(path.join(__dirname, "client", "dist", "index.html"));
+});
 
 
 connectDB().then(() => {
