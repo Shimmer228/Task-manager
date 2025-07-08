960ae0eea1f02b2962eeb9b855ab6a8731cbb20a
Rodion <rodson5973@gmail.com>
2025-07-08 21:45:26 +0300

    fix2002

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -1,4 +1,12 @@
 // client/vite.config.ts
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+import { dirname, resolve } from 'path';
+import { fileURLToPath } from 'url';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+
 export default defineConfig({
   plugins: [react()],
   build: {
